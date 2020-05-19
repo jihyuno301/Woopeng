4c6f79d441b90a2fa3829a0fc32c28ab10f9845c
bb_young <kimbbo0725@gmail.com>
2020-05-19 15:47:50 +0900

    no message

--- expressServer.js
+++ expressServer.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : '192.168.30.50',
   user     : 'woopeng',
   password : '1234',
   database : 'woopeng'
@@ -32,6 +32,9 @@ app.get('/login', function(req, res){
   res.render('login');
 })
 
+app.get('/mypage', function(req, res){
+  res.render('myPage'); 
+})
 app.get('/authResult', function(req, res){
   var authCode = req.query.code;
   console.log(authCode);
@@ -64,6 +67,67 @@ app.get('/authResult', function(req, res){
     }
   })
 })
+
+app.post('/list',auth, function(req, res){
+
+   // var userId = req.decoded.userId;
+   // var sql = "SELECT * FROM user WHERE id = ? ";
+  
+   var option = {
+    method : "POST",
+    url : "https://testapi.openbanking.or.kr/v2.0/transfer/deposit/acnt_num",
+    headers : {
+        Authorization : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJUOTkxNjI4OTQwIiwic2NvcGUiOlsib29iIl0sImlzcyI6Imh0dHBzOi8vd3d3Lm9wZW5iYW5raW5nLm9yLmtyIiwiZXhwIjoxNTk3NjM5MDY1LCJqdGkiOiJjZmYwZjE1Ny01MGEyLTRmNjYtOTA5MS00NTEyZDc5YTM4NWQifQ.wUE4i4IIGJUwtr1PwGbtZw_4M5Mz8hd7poLGM6Di2PE',
+        "Content-Type" : "application/json"
+    },
+    json : {
+      "cntr_account_type": "N",
+      "cntr_account_num": "9366449466",
+      "wd_pass_phrase": "NONE",
+      "wd_print_content": "환불금액", 
+      "name_check_option": "on",
+      "tran_dtime": "20200519140900",
+      "req_cnt": "1",
+      "req_list": [
+        {
+        "tran_no": "1",
+        "bank_tran_id": "T991628940U000001213",
+        "bank_code_std": "097",
+        "account_num": "123456789123",
+        "account_holder_name": "김보영",	
+        "print_content": "쇼핑몰환불",
+        "tran_amt": "10000",
+        "req_client_name": "홍길동",
+        "req_client_num": "HONGGILDONG1234",
+        "req_client_fintech_use_num" : "199162894057883856821429",
+        "transfer_purpose": "TR"
+        }
+      ]
+    }
+}
+request(option, function(err, response, body){
+    if(err){
+        console.error(err);
+        throw err;
+    }
+    else {
+       // var accessRequestResult = JSON.parse(body);
+        console.log(body);
+        //console.log(accessRequestResult);
+        //res.json(accessRequestResult)
+    }
+  })
+})
+    // connection.query(sql, [userId], function(err, result){
+    //   if(err){
+    //     console.error(err);
+    //     throw err;
+    //   }
+    //   else{
+    //     console.log(result);
+       
+    // })
+  
 
 app.post('/signup', function(req, res){
   //data req get db store
